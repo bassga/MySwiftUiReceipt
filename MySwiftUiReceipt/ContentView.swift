ec48c9710870a67b376dd31a0b977b4b119784fe
basega <bassmky19940208@gmail.com>
2023-12-21 23:48:57 +0900

    Initial Commit

---
+++ MySwiftUiReceipt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MySwiftUiReceipt
+//
+//  Created by 奥川幹也 on 2023/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MySwiftUiReceipt/MySwiftUiReceiptApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUiReceiptApp.swift
+//  MySwiftUiReceipt
+//
+//  Created by 奥川幹也 on 2023/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUiReceiptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
